c4c88981506ca870203c40c6921bf952b96bf8d4
hocdan <34260026+hocdan@users.noreply.github.com>
2021-06-13 06:00:41 -0400

    Update jogo.c

--- Raylib/The-way-of-Go/jogo.c
+++ Raylib/The-way-of-Go/jogo.c
@@ -7,12 +7,15 @@
 
 int jogo() {
     //constantes da janela Jogo
-    const int largura = 800;
+    const int largura = 900;
     const int altura = 700;
 
     //variaveis da janela Jogo
-    int bolaPreta = FALSE, bolaBranca = FALSE;
-    int status = 0; //status de retorno da janela ao ser fechada (status = encerrar como padrao)
+    int encerrar = FALSE;
+    int status = 1; //status de retorno da janela ao ser fechada (status = menu como padrao)
+
+    //inicializando botao de retorno
+    Rectangle botaoVoltar = {820, 20, 50, 50};
 
     //inicializando tela e FPS
     InitWindow(largura, altura, "Jogo - GO");
@@ -23,14 +26,24 @@ int jogo() {
     while (!WindowShouldClose()) {
         //Update
 
+        //verificando toque no botao de retornar
+        if ( CheckCollisionPointRec( GetMousePosition(), botaoVoltar) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            status = 1; //status = abrir janela Menu
+            encerrar = TRUE;
+        }
 
         //Draw
         BeginDrawing();
             ClearBackground(BLACK);
 
-            DrawText( FormatText("JANELA DO JOGO DE GO"), 400, 350, 40, RAYWHITE); //frase teste
+            DrawText( FormatText("JANELA DO JOGO DE GO"), 200, 350, 40, RAYWHITE); //frase teste
+
+            //imprimindo botao de de retorno
+            DrawRectangle(botaoVoltar.x, botaoVoltar.y, botaoVoltar.width, botaoVoltar.height, RAYWHITE);
 
         EndDrawing();
+
+        if (encerrar) break; //encerrando loop da janela atual
     }
 
     CloseWindow(); //encerrando janela Jogo
