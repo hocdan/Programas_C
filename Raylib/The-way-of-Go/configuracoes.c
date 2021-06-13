5eda282492e0d3cf72ebf93f13a044f3b46b6698
hocdan <34260026+hocdan@users.noreply.github.com>
2021-06-13 06:00:05 -0400

    Update configuracoes.c

--- Raylib/The-way-of-Go/configuracoes.c
+++ Raylib/The-way-of-Go/configuracoes.c
@@ -7,14 +7,15 @@
 
 int config() {
     //constantes da janela Configuracoes
-    const int largura = 800;
+    const int largura = 900;
     const int altura = 700;
 
     //variaveis da janela Configuracoes
-    int bolaPreta = FALSE, bolaBranca = FALSE;
+    int bolaPreta = FALSE, bolaBranca = FALSE, encerrar = FALSE;
     int status = 1; //status de retorno da janela ao ser fechada (status = menu como padrao)
 
-    //inicializando paineis e botoes do menu
+    //inicializando botao de retorno
+    Rectangle botaoVoltar = {820, 20, 50, 50};
 
     //inicializando tela e FPS
     InitWindow(largura, altura, "Configuracoes - GO");
@@ -25,17 +26,28 @@ int config() {
     while (!WindowShouldClose()) {
         //Update
 
+        //verificando toque no botao de retornar
+        if ( CheckCollisionPointRec( GetMousePosition(), botaoVoltar) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            status = 1; //status = abrir janela Menu
+            encerrar = TRUE;
+        }
+
         //Draw
         BeginDrawing();
             ClearBackground(BLACK);
 
-            DrawText( FormatText("JANELA DE CONFIGURACOES"), 400, 350, 40, RAYWHITE); //frase teste
+            DrawText( FormatText("JANELA DE CONFIGURACOES"), 200, 350, 40, RAYWHITE); //frase teste
+
+            //imprimindo botao de de retorno
+            DrawRectangle(botaoVoltar.x, botaoVoltar.y, botaoVoltar.width, botaoVoltar.height, RAYWHITE);
 
             //imprimindo efeito de bola branca/preta na ponta do mouse
             if (bolaPreta) DrawCircle( GetMousePosition().x, GetMousePosition().y, 10, BLACK);
             else if (bolaBranca) DrawCircle( GetMousePosition().x, GetMousePosition().y, 10, RAYWHITE);
 
         EndDrawing();
+
+        if (encerrar) break; //encerrando loop da janela atual
     }
 
     CloseWindow(); //encerrando janela Configuracoes
