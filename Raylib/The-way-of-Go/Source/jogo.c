fd0ea5925f2f560459bc6c71a819529544c3602b
hocdan <34260026+hocdan@users.noreply.github.com>
2021-10-14 22:45:01 -0300

    Update jogo.c

--- Raylib/The-way-of-Go/Source/jogo.c
+++ Raylib/The-way-of-Go/Source/jogo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "raylib.h"
 
 #include "manipularArquivos.h"
@@ -9,15 +10,60 @@
 
 int jogo() {
     //constantes da janela Jogo
-    const int largura = 900;
-    const int altura = 700;
+    int largura, altura, som, tamanho, handicap;
 
     //variaveis da janela Jogo
+    int i, j;
     int encerrar = FALSE;
     int status = 1; //status de retorno da janela ao ser fechada (status = menu como padrao)
+    char tamTela[11], tamTabuleiro[11], ativarSom[4], ativarHandicap[2];
 
-    //inicializando botao de retorno
-    Rectangle botaoVoltar = {820, 20, 50, 50};
+    //setando valores iniciais das variaveis da janela Jogo
+    enviarDado(tamTela, 0);
+    enviarDado(ativarSom, 1);
+    enviarDado(tamTabuleiro, 2);
+    enviarDado(ativarHandicap, 3);
+
+    if ( strcmp(tamTela, "500x300") == 0) {
+        largura = 500;
+        altura = 300;
+    } else if ( strcmp(tamTela, "700x500") == 0) {
+        largura = 700;
+        altura = 500;
+    } else if ( strcmp(tamTela, "900x700") == 0) {
+        largura = 900;
+        altura = 700;
+    }
+
+    if ( strcmp(ativarSom, "YES") == 0) som = TRUE;
+    else som = FALSE;
+
+    if ( strcmp(tamTabuleiro, "09x09") == 0) tamanho = 9;
+    else if ( strcmp(tamTela, "13x13") == 0) tamanho = 13;
+    else if ( strcmp(tamTela, "19x19") == 0) tamanho = 19;
+
+    handicap = atoi(ativarHandicap);
+
+    //inicializando botao de retorno (de acordo com as dimensoes atuais da janela)
+    Rectangle botaoVoltar;
+    if ( largura == 500 && altura == 300) {
+        botaoVoltar.width = 30;
+        botaoVoltar.height = 30;
+        botaoVoltar.x = 460;
+        botaoVoltar.y = 10;
+    }
+    else if ( largura == 700 && altura == 500) {
+        botaoVoltar.width = 40;
+        botaoVoltar.height = 40;
+        botaoVoltar.x = 640;
+        botaoVoltar.y = 20;
+    }
+    else {
+        botaoVoltar.width = 50;
+        botaoVoltar.height = 50;
+        botaoVoltar.x = 820;
+        botaoVoltar.y = 20;
+    }
 
     //inicializando tela e FPS
     InitWindow(largura, altura, "Jogo - GO");
@@ -38,11 +84,28 @@ int jogo() {
         BeginDrawing();
             ClearBackground(BLACK);
 
-            DrawText( FormatText("JANELA DO JOGO DE GO"), 200, 350, 40, RAYWHITE); //frase teste
+            //imprimindo tabuleiro
+            for (i=0; i<tamanho; i++) {
+                for (j=0; j<tamanho; j++) {
+                    DrawRectangle(j*10+10, i*10, 10, 10, YELLOW);
+                }
+            }
+
+            //imprimindo informacoes
+            DrawText( FormatText("Dimensões: %s", tamTela), 20, 20, 20, RAYWHITE);
+            DrawText( FormatText("Som: %s", ativarSom), 20, 60, 20, RAYWHITE);
+            DrawText( FormatText("Tamanho: %s", tamTabuleiro), 20, 100, 20, RAYWHITE);
+            DrawText( FormatText("Handicap: %s", ativarHandicap), 20, 140, 20, RAYWHITE);
 
             //imprimindo botao de de retorno
             DrawRectangle(botaoVoltar.x, botaoVoltar.y, botaoVoltar.width, botaoVoltar.height, RAYWHITE);
-            DrawText(FormatText("<"), botaoVoltar.x+16, botaoVoltar.y+8, 40, BLACK);
+            if ( largura == 500 && altura == 300) {
+                DrawText(FormatText("<"), botaoVoltar.x+10, botaoVoltar.y+5, 28, BLACK);
+            } else if ( largura == 700 && altura == 500) {
+                DrawText(FormatText("<"), botaoVoltar.x+14, botaoVoltar.y+7, 30, BLACK);
+            } else {
+                DrawText(FormatText("<"), botaoVoltar.x+16, botaoVoltar.y+8, 40, BLACK);
+            }
 
         EndDrawing();
 
